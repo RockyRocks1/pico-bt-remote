2f377516986fe3cad671f8121d365d17658f2993
RockyRocks1 <eferfawdwa@gmail.com>
2026-09-19 20:55:14 -0400

    Add: mouse wheel and keyboard hidreportdescriptors

--- pico/BleManager.cpp
+++ pico/BleManager.cpp
@@ -1,34 +1,68 @@
 #include "BleManager.hpp"
 
- // temporary, from the btstack example
 static const uint8_t hidReportDescriptor[] = {
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x02,                    // USAGE (Mouse)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, 0x01,                    // Report ID 1
-    0x09, 0x01,                    //   USAGE (Pointer)
-    0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x05, 0x09,                    //     USAGE_PAGE (Button)
-    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-    0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
-    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-    0x95, 0x03,                    //     REPORT_COUNT (3)
-    0x75, 0x01,                    //     REPORT_SIZE (1)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //     REPORT_COUNT (1)
-    0x75, 0x05,                    //     REPORT_SIZE (5)
-    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
-    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-    0x75, 0x08,                    //     REPORT_SIZE (8)
-    0x95, 0x02,                    //     REPORT_COUNT (2)
-    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
-    0xc0,                          //   END_COLLECTION
-    0xc0
+    0x05, 0x01,         //  USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,         //  USAGE (Mouse)
+    0xa1, 0x01,         //  COLLECTION (Application)
+    0x85, 0x01,         //      Report ID 1
+    0x09, 0x01,         //      USAGE (Pointer)
+    0xa1, 0x00,         //      COLLECTION (Physical)
+    0x05, 0x09,         //          USAGE_PAGE (Button)
+    0x19, 0x01,         //          USAGE_MINIMUM (Button 1)
+    0x29, 0x03,         //          USAGE_MAXIMUM (Button 3)
+    0x15, 0x00,         //          LOGICAL_MINIMUM (0)
+    0x25, 0x01,         //          LOGICAL_MAXIMUM (1)
+    0x95, 0x03,         //          REPORT_COUNT (3)
+    0x75, 0x01,         //          REPORT_SIZE (1)
+    0x81, 0x02,         //          INPUT (Data,Var,Abs)
+    0x95, 0x01,         //          REPORT_COUNT (1)
+    0x75, 0x05,         //          REPORT_SIZE (5)
+    0x81, 0x03,         //          INPUT (Cnst,Var,Abs)
+    0x05, 0x01,         //          USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,         //          USAGE (X)
+    0x09, 0x31,         //          USAGE (Y)
+    0x09, 0x38,         //          USAGE (WHEEL)
+    0x15, 0x81,         //          LOGICAL_MINIMUM (-127)
+    0x25, 0x7f,         //          LOGICAL_MAXIMUM (127)
+    0x75, 0x08,         //          REPORT_SIZE (8)
+    0x95, 0x02,         //          REPORT_COUNT (3)
+    0x81, 0x06,         //          INPUT (Data,Var,Rel)
+    0xc0,               //      END_COLLECTION
+    0xc0,               //  END_COLLECTION
+    0x05, 0x01,         //  USAGE_PAGE (Generic Desktop)
+    0x09, 0x06,         //  USAGE (Keyboard)
+    0xa1, 0x01,         //  COLLECTION (Application)
+    0x85, 0x02,         //      Report ID 2
+    0x75, 0x01,         //      REPORT_SIZE (1)
+    0x95, 0x08,         //      REPORT_COUNT (8)
+    0x05, 0x07,         //      USAGE_PAGE (Key codes)
+    0x19, 0xe0,         //      USAGE_MINIMUM (Keyboard LeftControl)
+    0x29, 0xe7,         //      USAGE_MAXIMUM (Keyboard Right GUI)
+    0x15, 0x00,         //      LOGICAL_MINIMUM (0)
+    0x25, 0x01,         //      LOGICAL_MAXIMUM (1)
+    0x81, 0x02,         //      INPUT (Data,Var,Abs)
+    0x75, 0x01,         //      REPORT_SIZE (1)
+    0x95, 0x08,         //      REPORT_COUNT (8)
+    0x81, 0x03,         //      INPUT (Cnst,Var,Abs)
+    0x95, 0x05,         //      REPORT_COUNT (5)
+    0x75, 0x01,         //      REPORT_SIZE (1)
+    0x05, 0x08,         //      USAGE_PAGE (LEDs)
+    0x19, 0x01,         //      USAGE_MINIMUM (Num Lock)
+    0x29, 0x05,         //      USAGE_MAXIMUM (Kana)
+    0x91, 0x02,         //      OUTPUT (Data,Var,Abs)
+    0x95, 0x01,         //      REPORT_COUNT (1)
+    0x75, 0x03,         //      REPORT_SIZE (3)
+    0x91, 0x03,         //      OUTPUT (Cnst,Var,Abs)
+    0x95, 0x06,         //      REPORT_COUNT (6)
+    0x75, 0x08,         //      REPORT_SIZE (8)
+    0x15, 0x00,         //      LOGICAL_MINIMUM (0)
+    0x25, 0xff,         //      LOGICAL_MAXIMUM (255)
+    0x05, 0x07,         //      USAGE_PAGE (Key codes)
+    0x19, 0x00,         //      USAGE_MINIMUM (Reserved (no event indicated))
+    0x29, 0xff,         //      USAGE_MAXIMUM (Reserved)
+    0x81, 0x00,         //      INPUT (Data, Array)
+    0xc0,               //  END_COLLECTION
+
 };
 
 // also temporary
@@ -69,7 +103,8 @@ void BleManager::SendMouseReport(MousePayload& mouse) {
     if (m_hidCid == HCI_CON_HANDLE_INVALID)
         return;
 
-    uint8_t report[4] = {
+    uint8_t report[5] = {
+        0x01,
         mouse.buttonMask,
         static_cast<uint8_t>(mouse.dx),
         static_cast<uint8_t>(mouse.dy),
@@ -82,7 +117,8 @@ void BleManager::SendKeyboardReport(KeyboardPayload& keyboard) {
     if (m_hidCid == HCI_CON_HANDLE_INVALID)
         return;
 
-    uint8_t report[8] = {
+    uint8_t report[9] = {
+        0x02,
         keyboard.modifierMask,
         keyboard.reserved,
         keyboard.keys[0],
